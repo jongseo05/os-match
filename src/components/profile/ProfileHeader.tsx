481b42ac78c719ff4eedecfed20b829afde61917
jongseo05 <jourxy05@naver.com>
2025-05-12 18:35:59 +0900

    기본 프로필 이미지 변경경

--- src/components/profile/ProfileHeader.tsx
+++ src/components/profile/ProfileHeader.tsx
@@ -61,7 +61,7 @@ export default function ProfileHeader({ userData, onUpdate }: ProfileHeaderProps
           <div className="relative group">
             <div className="w-24 h-24 rounded-full overflow-hidden border-4 border-black bg-black">
               <Image
-                src={previewImage || userData.profileImage}
+                src={previewImage || "/dev_profile.png"}
                 alt="Profile"
                 width={96}
                 height={96}
