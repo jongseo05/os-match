d38f661f92913b0d574a1cf85580511954bd3585
jongseo05 <jourxy05@naver.com>
2025-05-14 00:20:40 +0900

    deploy test1

--- next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  eslint: {
-    // 빌드 시 경고는 허용하고 오류만 실패하도록 설정
-    ignoreDuringBuilds: true,
-  },
-};
-
-module.exports = nextConfig;
